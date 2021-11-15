79598988736163b6c7712853bafdd1189ef2724e
NabucodonosorII <91675628+NabucodonosorII@users.noreply.github.com>
2021-11-15 06:21:53 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+    int f1 = 1, f2 = 1, fn = 0, n;
+    cin >> n;
+    if(n==0){
+    }else if(n!=0){
+        cout << f1 << endl << f2 << endl;fn = f1 + f2;
+        while(fn <= n) {
+            cout<<fn<<endl;f1=f2;f2=fn;fn=f1 + f2;
+        }
+    }
+    return 0;
 }
